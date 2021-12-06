2e39bc9c1c48dd065623f6147803e489dfd162ac
jennijavanainen <jenni.kupukupu@gmail.com>
2021-12-06 17:12:12 +0200

    Navbar basic functionality works.

--- app/src/main/java/fi/javanainen/migraineapp/HistoryActivity.java
+++ app/src/main/java/fi/javanainen/migraineapp/HistoryActivity.java
@@ -1,14 +1,42 @@
 package fi.javanainen.migraineapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+
 
 public class HistoryActivity extends AppCompatActivity {
+    private BottomNavigationView navBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
+
+        // Navbar functionality
+        navBar = findViewById(R.id.bottom_navigation);
+        navBar.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.main:
+                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        return true;
+                    case R.id.history:
+                        return true;
+                    case R.id.settings:
+                        startActivity(new Intent(getApplicationContext(), SettingsActivity.class));
+                        return true;
+                }
+                return false;
+            }
+        });
     }
 }
--- app/src/main/java/fi/javanainen/migraineapp/SettingsActivity.java
+++ app/src/main/java/fi/javanainen/migraineapp/SettingsActivity.java
@@ -1,14 +1,40 @@
 package fi.javanainen.migraineapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class SettingsActivity extends AppCompatActivity {
+    private BottomNavigationView navBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        // Navbar functionality
+        navBar = findViewById(R.id.bottom_navigation);
+        navBar.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.main:
+                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        return true;
+                    case R.id.history:
+                        startActivity(new Intent(getApplicationContext(), HistoryActivity.class));
+                        return true;
+                    case R.id.settings:
+                        return true;
+                }
+                return false;
+            }
+        });
     }
 }
