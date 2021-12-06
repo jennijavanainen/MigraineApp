1c3402ea3c1608cddd67a19cc2e9e6cdd6988a5d
jennijavanainen <jenni.kupukupu@gmail.com>
2021-12-06 14:07:47 +0200

    Testing with bottom navbar

--- app/src/main/java/fi/javanainen/migraineapp/HistoryActivity.java
+++ app/src/main/java/fi/javanainen/migraineapp/HistoryActivity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class StatsActivity extends AppCompatActivity {
+public class HistoryActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_stats2);
+        setContentView(R.layout.activity_history);
     }
 }
--- app/src/main/java/fi/javanainen/migraineapp/MainActivity.java
+++ app/src/main/java/fi/javanainen/migraineapp/MainActivity.java
@@ -1,8 +1,14 @@
 package fi.javanainen.migraineapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.fragment.app.Fragment;
+import androidx.annotation.NonNull;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.MenuItem;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //BottomNavigationView navBar = findViewById(R.id.bottom_navigation);
+        //navBar.setOnNavigationItemSelectedListener(navListener);
     }
+
+    public void addNewMigraineButtonClicked(View view) {
+        Intent intent = new Intent(this, AddMigraineActivity.class);
+        startActivity(intent);
+    }
+
+
 }
