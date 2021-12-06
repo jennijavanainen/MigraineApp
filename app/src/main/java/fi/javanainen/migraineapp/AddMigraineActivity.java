bc8ca39da9392a248ee52c8d161a397d1e84cbc5
jennijavanainen <jenni.kupukupu@gmail.com>
2021-12-06 14:07:47 +0200

    Testing with bottom navbar

--- app/src/main/java/fi/javanainen/migraineapp/AddMigraineActivity.java
+++ app/src/main/java/fi/javanainen/migraineapp/AddMigraineActivity.java
@@ -1,15 +1,25 @@
 package fi.javanainen.migraineapp;
 
 /**
- *
+ * Activity is responsible for adding a new Migraine if no active Migraine exists.
+ * Activity will also create a single MigraineEvent. The user chooses the parameters.
+ * If active Migraine exists, the activity will use it and create only a new MigraineEvent.
+ * @author Jenni Javanainen
  */
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import java.util.ArrayList;
 
 public class AddMigraineActivity extends AppCompatActivity {
+
+    // Migraine attributes
+    private boolean activeMigraineExists;
     private MigraineList migraineList;
     private ArrayList<String> triggers;
     private Date date;
@@ -21,21 +31,46 @@ public class AddMigraineActivity extends AppCompatActivity {
     private MigraineEvent event;
     private Migraine migraine;
 
+    // Views
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_migraine);
         migraineList = MigraineList.getInstance();
-
+        // get activemigraineexists
     }
 
-    public void sendButtonClicked() {
-        date = new Date(4, 12, 2021);
-        time = new Time(14, 06);
+    // Adding triggers
+    // Adding pain
+    // Adding symptoms
+    // Adding medicines
+    // Adding treatments
+
+
+    /**
+     * Method creates Date, Time and MigraineEvent objects with parameters given by the user.
+     * If active Migraine does not exist, the method creates it as well.
+     * After this the Activity closes and user returns to MainActivity with information about activeMigraineExists.
+     * @param view saveButton
+     */
+    public void saveButtonClicked(View view) {
+        date = new Date(4, 12, 2021);   // Tiedot haetaan
+        time = new Time(14, 06);           // Tiedot haetaan
         event = new MigraineEvent(date, time, pain, symptoms, medicines, treatments);
-        // ehtolause, joka tarkistaa luodaanko migreeniä vai pelkkä eventti
-        migraine = new Migraine(triggers, event);
-        migraineList.addMigraine(migraine);
+        if (activeMigraineExists) {
+            migraineList.returnLast().addEvent(date, time, pain, symptoms, medicines, treatments);
+        } else {
+            migraine = new Migraine(triggers, event);
+            migraineList.addMigraine(migraine);
+            activeMigraineExists = true;
+        }
+        // Save info to database
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+
     }
 
 
