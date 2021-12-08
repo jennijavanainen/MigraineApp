4a5931c8da98c68d8bebb01fe8b6bb9bcef45f04
jennijavanainen <jenni.kupukupu@gmail.com>
2021-12-08 18:24:46 +0200

    Date and Time picker widget improvements

--- app/src/main/java/fi/javanainen/migraineapp/AddMigraineActivity.java
+++ app/src/main/java/fi/javanainen/migraineapp/AddMigraineActivity.java
@@ -18,6 +18,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.TimePicker;
 
 import java.util.ArrayList;
@@ -39,8 +40,7 @@ public class AddMigraineActivity extends AppCompatActivity {
     private Migraine migraine;
 
     // Views
-    Button btnDatePicker, btnTimePicker;
-    EditText txtDate, txtTime;
+    TextView txtDate, txtTime;
     private int mYear, mMonth, mDay, mHour, mMinute;
 
 
@@ -49,11 +49,16 @@ public class AddMigraineActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_migraine);
         migraineList = MigraineList.getInstance();
-
-        btnDatePicker=(Button)findViewById(R.id.btn_date);
-        btnTimePicker=(Button)findViewById(R.id.btn_time);
-        txtDate=(EditText)findViewById(R.id.in_date);
-        txtTime=(EditText)findViewById(R.id.in_time);
+        txtDate=(TextView) findViewById(R.id.in_date);
+        txtTime=(TextView) findViewById(R.id.in_time);
+        final Calendar c = Calendar.getInstance();
+        mYear = c.get(Calendar.YEAR);
+        mMonth = c.get(Calendar.MONTH);
+        mDay = c.get(Calendar.DAY_OF_MONTH);
+        txtDate.setText(mDay + "." + mMonth + "." +mYear);
+        mHour = c.get(Calendar.HOUR_OF_DAY);
+        mMinute = c.get(Calendar.MINUTE);
+        txtTime.setText(mHour + "." + mMinute);
 
         // get activemigraineexists
     }
@@ -95,20 +100,10 @@ public class AddMigraineActivity extends AppCompatActivity {
      * @param view View
      */
     public void selectDate(View view) {
-        final Calendar c = Calendar.getInstance();
-        mYear = c.get(Calendar.YEAR);
-        mMonth = c.get(Calendar.MONTH);
-        mDay = c.get(Calendar.DAY_OF_MONTH);
-
         DatePickerDialog datePickerDialog = new DatePickerDialog(this,
-                new DatePickerDialog.OnDateSetListener() {
-                    @Override
-                    public void onDateSet(DatePicker view, int year,
-                                          int monthOfYear, int dayOfMonth) {
-                        txtDate.setText(dayOfMonth + "." + (monthOfYear + 1) + "." + year);
-                    }
-                }, mYear, mMonth, mDay);
+                (view1, year, monthOfYear, dayOfMonth) -> txtDate.setText(dayOfMonth + "." + (monthOfYear + 1) + "." + year), mYear, mMonth, mDay);
         datePickerDialog.show();
+
     }
 
     /**
@@ -116,18 +111,8 @@ public class AddMigraineActivity extends AppCompatActivity {
      * @param view View
      */
     public void selectTime(View view) {
-        final Calendar c = Calendar.getInstance();
-        mHour = c.get(Calendar.HOUR_OF_DAY);
-        mMinute = c.get(Calendar.MINUTE);
-
         TimePickerDialog timePickerDialog = new TimePickerDialog(this,
-                new TimePickerDialog.OnTimeSetListener() {
-                    @Override
-                    public void onTimeSet(TimePicker view, int hourOfDay,
-                                          int minute) {
-                        txtTime.setText(hourOfDay + "." + minute);
-                    }
-                }, mHour, mMinute, true);
+                (view1, hourOfDay, minute) -> txtTime.setText(hourOfDay + "." + minute), mHour, mMinute, true);
         timePickerDialog.show();
     }
 
