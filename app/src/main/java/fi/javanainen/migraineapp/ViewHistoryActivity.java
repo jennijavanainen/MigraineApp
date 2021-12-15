b171f2a51eb361a6877582989a0a72d7f7901866
teemu-pennanen <teemu.pennanen@metropolia.fi>
2021-12-15 19:32:29 +0200

    Teemun javadoc kommentit

--- app/src/main/java/fi/javanainen/migraineapp/ViewHistoryActivity.java
+++ app/src/main/java/fi/javanainen/migraineapp/ViewHistoryActivity.java
@@ -53,8 +53,6 @@ public class ViewHistoryActivity extends AppCompatActivity {
         GraphView graph = (GraphView) findViewById(R.id.graph);
         LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[]{
 
-                //ADD DATA
-
                 new DataPoint(0, 1),
                 new DataPoint(1, 5),
                 new DataPoint(2, 3),
